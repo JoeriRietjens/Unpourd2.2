15e62980b1fab2cef3c0a82cc12d7edc179c59e6
JoeriRietjens <joeririetjens@gmail.com>
2019-12-13 14:48:32 +0100

    added unpourd classlib

---
+++ Unpourd/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Unpourd.Models;
+using Logic;
+
+namespace Unpourd.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly PostManager _mgr;
+
+        public HomeController(PostManager context)
+        {
+            _mgr = context;
+        }
+
+        public IActionResult HomePage()
+        {
+            _mgr.GetAllPosts();
+
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Unpourd/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unpourd.Models
+{
+    public class Post
+    {
+        public int Id { get; }
+        public string CoffeType { get; }
+        public string Username { get; }
+
+    }
+}
